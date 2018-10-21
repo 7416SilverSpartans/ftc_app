cb5a2424ff2ec77e80aa7151de96a2890463f843
7416SilverSpartans <silverspartans7416@gmail.com>
2018-10-21 19:05:24 -0400

    Got drive train working.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverOP_7416.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverOP_7416.java
@@ -15,7 +15,6 @@ public class DriverOP_7416 extends LinearOpMode {
     private DcMotor right_drive = null;
     private DcMotor lift = null;
     private Servo dropper = null;
-    @Override
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -26,14 +25,19 @@ public class DriverOP_7416 extends LinearOpMode {
         dropper = hardwareMap.get(Servo.class, "dropper");
 
         lift.setDirection(DcMotorSimple.Direction.FORWARD);
-        right_drive.setDirection(DcMotorSimple.Direction.FORWARD);
+        right_drive.setDirection(DcMotorSimple.Direction.REVERSE);
         left_drive.setDirection(DcMotorSimple.Direction.FORWARD);
+        waitForStart();
         while(opModeIsActive()) {
             if (gamepad1.left_stick_y != 0) {
                 left_drive.setPower(Range.clip(gamepad1.left_stick_y, -1.0, 1.0));
+            } else {
+                left_drive.setPower(0);
             }
             if (gamepad1.right_stick_y != 0) {
                 right_drive.setPower(Range.clip(gamepad1.right_stick_y, -1.0, 1.0));
+            } else {
+                right_drive.setPower(0);
             }
             if (gamepad2.right_stick_y != 0) {
                 lift.setPower(Range.clip(gamepad2.right_stick_y, -1.0, 1.0));
@@ -41,7 +45,6 @@ public class DriverOP_7416 extends LinearOpMode {
             //will need to fix once we see what is wrong. works but numbers for position will need to change.
             if (gamepad2.a) {
                 dropper.setPosition(1.0);
-                sleep(2000);
                 dropper.setPosition(0.0);
             }
         }
